d1f60bcb8165ded165c9599e1c8d551ad352f143
MrJorgeXavier <mr.jorge.xavier@gmail.com>
2021-04-28 18:30:33 -0300

    Creating apple tv target

---
+++ projects/ios/Sample/Sample-AppleTv/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Sample-AppleTv
+//
+//  Created by ubook on 28/04/21.
+//  Copyright © 2021 PRSoluções. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
